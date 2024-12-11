14318168c4e0011c8326702925d833359a0cdb86
Tom Giagtzoglou <tom.giagtzoglou@gmail.com>
2024-12-10 22:23:29 -0500

    day 4 part 2

--- 2024/04/solution.py
+++ 2024/04/solution.py
@@ -66,14 +66,46 @@ def searchForXmas(array, center):
     return numHits
 
 
+MAS = "MAS"
+
+diags = [
+    [1, 1],
+    [-1, 1],
+    [-1, -1],
+    [1, -1]
+]
+
+
+def searchForMas(array, center):
+    if center[0] - 1 < 0 or center[0] + 1 >= len(array):
+        return 0
+    elif center[1] - 1 < 0 or center[1] + 1 >= len(array[center[0]]):
+        return 0
+    goodDiags = 0
+    for i, diag in enumerate([[1, 1], [-1, 1]]):
+        val = array[center[0] + diag[0]][center[1] + diag[1]]
+        if not (val == "M" or val == "S"):
+            return 0
+        otherSide = diags[(i + 2) % 4]
+        otherVal = array[center[0] + otherSide[0]][center[1] + otherSide[1]]
+        if val == "M" and otherVal == "S":
+            goodDiags += 1
+        if val == "S" and otherVal == "M":
+            goodDiags += 1
+
+    #
+    return goodDiags == 2
+
+
 def main():
     input = read_input()
     array = input.splitlines()
-
     array = [list(i) for i in array]
+
+    # part 1
     dispArray = copy.deepcopy(array)
     totalXmas = 0
-    # print2DArray(array)
+
     for i in range(len(array)):
         for j in range(len(array[i])):
             if array[i][j] == 'X':
@@ -83,8 +115,19 @@ def main():
                     totalXmas += numHits
         # print("checked row " + "".join(array[i]))
 
-    # print2DArray(dispArray)
     print(totalXmas)
+
+    # part 2
+    totalMas = 0
+    for i in range(len(array)):
+        for j in range(len(array[i])):
+            if array[i][j] == 'A':
+                numHits = searchForMas(array, [i, j])
+                if numHits > 0:
+                    # dispArray[i][j] = str(numHits)
+                    totalMas += numHits
+
+    print(totalMas)
 
 
 if __name__ == "__main__":
